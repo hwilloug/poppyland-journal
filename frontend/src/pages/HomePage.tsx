d8e8c3dedcfaaeb59b6a69c33576da22075f34b3
Hannah Willoughby <hannah@tcah.com>
2023-11-03 12:10:28 -0400

    Get hello world running for frontend

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FunctionComponent = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    )
+  
+}
+  
+export default HomePage
