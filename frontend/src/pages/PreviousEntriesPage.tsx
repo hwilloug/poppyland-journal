ddab24a0d26019f34d63566e93c5a30a8dddec9b
Hannah Willoughby <hannah@tcah.com>
2024-05-04 15:48:44 -0400

    Add month navigation to previous entries page

--- frontend/src/pages/PreviousEntriesPage.tsx
+++ frontend/src/pages/PreviousEntriesPage.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from "react"
+import React, { ReactNode, useMemo, useState } from "react"
 import { PageContentContainer } from "../components/shared-components/styled-components"
-import axios from "axios"
-import { apiEndpoints } from "../api-endpoints"
 import styled from "@emotion/styled"
 import MarkdownComponent from "../components/shared-components/Markdown"
 import SentimentVerySatisfiedIcon from "@mui/icons-material/SentimentVerySatisfied"
@@ -11,7 +9,6 @@ import SentimentDissatisfiedIcon from "@mui/icons-material/SentimentDissatisfied
 import SentimentVeryDissatisfiedIcon from "@mui/icons-material/SentimentVeryDissatisfied"
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react"
 import LoadingComponent from "../components/shared-components/Loading"
-import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { State, journalActions } from "../store"
 import { getProfile } from "../utils/get-profile"
@@ -25,16 +22,36 @@ import {
   Paper,
   Snackbar,
   Typography,
+  useTheme,
 } from "@mui/material"
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz"
 import {
   convertToLongDateFromShortDate,
   convertToShortDate,
 } from "../utils/date-utils"
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineDot,
+  TimelineConnector,
+  TimelineContent,
+} from "@mui/lab"
+import { JournalEntry } from "../types/journal-types"
+import { Link } from "react-router-dom"
+import { HashLink } from "react-router-hash-link"
+
+const ContentContainer = styled.div`
+  display: flex;
+`
+
+const NavigatorContainer = styled.div``
 
 const EntriesContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 80%;
+  margin-left: 175px;
   gap: 50px;
 `
 
@@ -93,6 +110,7 @@ const EntryFooterContainer = styled.div`
 const PreviousEntriesPage: React.FunctionComponent = () => {
   const { user, getAccessTokenSilently } = useAuth0()
   const dispatch = useDispatch()
+  const theme = useTheme()
   const userId = useSelector((state: State) => state.user.userId)
   const preferences = useSelector((state: State) => state.user.preferences)
   const journalName = useSelector((state: State) => state.user.journalName)
@@ -100,13 +118,43 @@ const PreviousEntriesPage: React.FunctionComponent = () => {
     getProfile(user!.sub!, dispatch, getAccessTokenSilently)
   }
 
-  const entries = useSelector((state: State) => {
+  const data = useSelector((state: State) => state.journal.entries)
+  const entries = useMemo(() => {
     let entries = []
-    for (let date in state.journal.entries) {
-      entries.push(state.journal.entries[date])
+    for (let date in data) {
+      entries.push(data[date])
     }
     return entries
-  })
+  }, [data])
+  const entriesByMonth = useMemo(() => {
+    const entriesByMonth: { [monthYear: string]: JournalEntry[] } = {}
+    for (let i = 0; i < entries.length; i++) {
+      const date = new Date(entries[i].date)
+      const months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ]
+      const month = months[date.getMonth()]
+      const year = date.getFullYear()
+      const monthYear = `${month} ${year}`
+      if (Object.keys(entriesByMonth).includes(monthYear)) {
+        entriesByMonth[monthYear] = [...entriesByMonth[monthYear], entries[i]]
+      } else {
+        entriesByMonth[monthYear] = [entries[i]]
+      }
+    }
+    return entriesByMonth
+  }, [entries])
   const isLoading = useSelector((state: State) => state.journal.isLoading)
 
   const [snackbarOpen, setSnackbarOpen] = useState<boolean>(false)
@@ -178,157 +226,215 @@ const PreviousEntriesPage: React.FunctionComponent = () => {
     setAnchorEl(null)
   }
 
+  const Navigator: React.FC = () => {
+    const monthYears = Object.keys(entriesByMonth)
+    return (
+      <Timeline position="left" sx={{ position: "fixed", width: "225px" }}>
+        {monthYears.map((m, i) => (
+          <HashLink
+            to={`#${m.replace(" ", "-")}`}
+            style={{
+              textDecoration: "none",
+              color: theme.palette.primary.main,
+            }}
+          >
+            <TimelineItem key={m}>
+              <TimelineSeparator>
+                <TimelineDot color="primary" />
+                {i !== monthYears.length - 1 && <TimelineConnector />}
+              </TimelineSeparator>
+              <TimelineContent>{m}</TimelineContent>
+            </TimelineItem>
+          </HashLink>
+        ))}
+      </Timeline>
+    )
+  }
+
   return (
     <PageContentContainer>
       <Typography variant="h4" sx={{ m: "20px 0" }} align="center">
         {journalName || "My Journal"}
       </Typography>
-      <EntriesContainer>
-        {!isLoading && entries.length === 0 && (
-          <NoEntriesContainer>
-            No entries yet! Head to <Link to="/today">today's entry page</Link>{" "}
-            to get started.
-          </NoEntriesContainer>
-        )}
-        {!isLoading &&
-          entries.map((entry) => (
-            <EntryContainer key={entry.date}>
-              <Typography variant="h6">
-                {convertToLongDateFromShortDate(entry.date)}
-              </Typography>
-              <hr />
-              {preferences.showMood && entry.mood && (
-                <MoodContainer>
-                  <Typography>Mood: </Typography>
-                  {getMoodIcon(entry.mood)}
-                </MoodContainer>
-              )}
-              {preferences.showSleep && entry.hoursSleep && (
-                <SectionContainer>
-                  <Typography>Hours sleep: {entry.hoursSleep}</Typography>
-                </SectionContainer>
-              )}
-              {preferences.showSleep && entry.sleepQuality && (
-                <SectionContainer>
-                  <Typography>Sleep Quality: {entry.sleepQuality}</Typography>
-                </SectionContainer>
-              )}
-              {preferences.showDailyAffirmation && entry.affirmation && (
-                <SectionContainer>
-                  <Typography>Daily Affirmation:</Typography>
-                  <MarkdownComponent view="view" value={entry.affirmation} />
-                </SectionContainer>
-              )}
-              {preferences.showDailyGoal && entry.goal && (
-                <SectionContainer>
-                  <Typography>Daily Goal:</Typography>
-                  <MarkdownComponent view="view" value={entry.goal} />
-                </SectionContainer>
-              )}
-              {preferences.showDailyQuestion && entry.dailyQuestionA && (
-                <SectionContainer>
-                  <Typography>
-                    Daily Question: {entry.dailyQuestionQ}
-                  </Typography>
-                  <MarkdownComponent view="view" value={entry.dailyQuestionA} />
-                </SectionContainer>
-              )}
-              {preferences.showMentalHealth &&
-                entry.mentalHealth.length > 0 && (
-                  <SectionContainer>
-                    <Typography>
-                      Mental Health:{" "}
-                      {entry.mentalHealth && entry.mentalHealth.join(", ")}
-                    </Typography>
-                  </SectionContainer>
-                )}
-              {preferences.showSubstance && entry.substances.length > 0 && (
-                <SectionContainer>
-                  <Typography>
-                    Substances:{" "}
-                    {entry.substances && entry.substances.join(", ")}
-                  </Typography>
-                </SectionContainer>
-              )}
-              {preferences.showExercise && (
-                <SectionContainer>
-                  <Typography>Minutes Exercise: {entry.exercise}</Typography>
-                </SectionContainer>
-              )}
-              {entry.entryContent && (
-                <MarkdownComponent view="view" value={entry.entryContent} />
-              )}
-              <EntryFooterContainer>
-                <Button
-                  id="more-button"
-                  onClick={(e) => handleMoreClick(e, entry.date)}
+      <ContentContainer>
+        <NavigatorContainer>
+          <Navigator />
+        </NavigatorContainer>
+        <EntriesContainer>
+          {!isLoading && entries.length === 0 && (
+            <NoEntriesContainer>
+              No entries yet! Head to{" "}
+              <Link to="/today">today's entry page</Link> to get started.
+            </NoEntriesContainer>
+          )}
+          {!isLoading &&
+            Object.keys(entriesByMonth).map((m) => (
+              <>
+                <Typography
+                  variant="h5"
+                  id={m.replace(" ", "-")}
+                  sx={{ mx: "auto" }}
                 >
-                  <MoreHorizIcon />
-                </Button>
-              </EntryFooterContainer>
-            </EntryContainer>
-          ))}
-        <Menu
-          anchorEl={anchorEl}
-          open={menuOpen}
-          onClose={handleMoreClose}
-          MenuListProps={{ "aria-labelledby": "more-button" }}
-        >
-          <MenuItem onClick={handleMoreClose}>
-            <Link
-              to={`/edit/${selectedDate}`}
-              style={{ textDecoration: "None", color: "black" }}
-            >
-              Edit Entry
-            </Link>
-          </MenuItem>
-          <MenuItem
-            onClick={() => {
-              setDeleteEntryDate(selectedDate!)
-              setIsModalOpen(true)
-              handleMoreClose()
-            }}
+                  {m}
+                </Typography>
+                {entriesByMonth[m].map((entry) => (
+                  <EntryContainer key={entry.date}>
+                    <Typography variant="h6">
+                      {convertToLongDateFromShortDate(entry.date)}
+                    </Typography>
+                    <hr />
+                    {preferences.showMood && entry.mood && (
+                      <MoodContainer>
+                        <Typography>Mood: </Typography>
+                        {getMoodIcon(entry.mood)}
+                      </MoodContainer>
+                    )}
+                    {preferences.showSleep && entry.hoursSleep && (
+                      <SectionContainer>
+                        <Typography>
+                          Hours slept: {parseFloat(entry.hoursSleep).toFixed(2)}
+                        </Typography>
+                      </SectionContainer>
+                    )}
+                    {preferences.showSleep && entry.sleepQuality && (
+                      <SectionContainer>
+                        <Typography>
+                          Sleep Quality: {entry.sleepQuality}
+                        </Typography>
+                      </SectionContainer>
+                    )}
+                    {preferences.showDailyAffirmation && entry.affirmation && (
+                      <SectionContainer>
+                        <Typography>Daily Affirmation:</Typography>
+                        <MarkdownComponent
+                          view="view"
+                          value={entry.affirmation}
+                        />
+                      </SectionContainer>
+                    )}
+                    {preferences.showDailyGoal && entry.goal && (
+                      <SectionContainer>
+                        <Typography>Daily Goal:</Typography>
+                        <MarkdownComponent view="view" value={entry.goal} />
+                      </SectionContainer>
+                    )}
+                    {preferences.showDailyQuestion && entry.dailyQuestionA && (
+                      <SectionContainer>
+                        <Typography>
+                          Daily Question: {entry.dailyQuestionQ}
+                        </Typography>
+                        <MarkdownComponent
+                          view="view"
+                          value={entry.dailyQuestionA}
+                        />
+                      </SectionContainer>
+                    )}
+                    {preferences.showMentalHealth &&
+                      entry.mentalHealth.length > 0 && (
+                        <SectionContainer>
+                          <Typography>
+                            Mental Health:{" "}
+                            {entry.mentalHealth &&
+                              entry.mentalHealth.join(", ")}
+                          </Typography>
+                        </SectionContainer>
+                      )}
+                    {preferences.showSubstance &&
+                      entry.substances.length > 0 && (
+                        <SectionContainer>
+                          <Typography>
+                            Substances:{" "}
+                            {entry.substances && entry.substances.join(", ")}
+                          </Typography>
+                        </SectionContainer>
+                      )}
+                    {preferences.showExercise && (
+                      <SectionContainer>
+                        <Typography>
+                          Minutes Exercise: {entry.exercise}
+                        </Typography>
+                      </SectionContainer>
+                    )}
+                    {entry.entryContent && (
+                      <MarkdownComponent
+                        view="view"
+                        value={entry.entryContent}
+                      />
+                    )}
+                    <EntryFooterContainer>
+                      <Button
+                        id="more-button"
+                        onClick={(e) => handleMoreClick(e, entry.date)}
+                      >
+                        <MoreHorizIcon />
+                      </Button>
+                    </EntryFooterContainer>
+                  </EntryContainer>
+                ))}
+              </>
+            ))}
+          <Menu
+            anchorEl={anchorEl}
+            open={menuOpen}
+            onClose={handleMoreClose}
+            MenuListProps={{ "aria-labelledby": "more-button" }}
           >
-            Delete Entry
-          </MenuItem>
-        </Menu>
-        <Modal
-          open={isModalOpen}
-          onClose={handleModalClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Box sx={modalStyle}>
-            <Typography id="modal-modal-title" variant="h6" component="h2">
-              Are you sure you want to delete this entry?
-            </Typography>
-            <Typography>
-              {convertToLongDateFromShortDate(deleteEntryDate)}
-            </Typography>
-            <ModalButtonsContainer>
-              <Button variant="contained" onClick={() => handleModalClose()}>
-                No, take me back
-              </Button>
-              <Button
-                color="error"
-                onClick={() => handleDeleteEntry(deleteEntryDate)}
+            <MenuItem onClick={handleMoreClose}>
+              <Link
+                to={`/edit/${selectedDate}`}
+                style={{ textDecoration: "None", color: "black" }}
               >
-                Yes, Delete
-              </Button>
-            </ModalButtonsContainer>
-          </Box>
-        </Modal>
-        <Snackbar
-          open={snackbarOpen}
-          autoHideDuration={6000}
-          onClose={handleSnackbarClose}
-        >
-          <Alert onClose={handleSnackbarClose} severity="success">
-            {snackbarMessage}
-          </Alert>
-        </Snackbar>
-        {isLoading && <LoadingComponent />}
-      </EntriesContainer>
+                Edit Entry
+              </Link>
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setDeleteEntryDate(selectedDate!)
+                setIsModalOpen(true)
+                handleMoreClose()
+              }}
+            >
+              Delete Entry
+            </MenuItem>
+          </Menu>
+          <Modal
+            open={isModalOpen}
+            onClose={handleModalClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyle}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Are you sure you want to delete this entry?
+              </Typography>
+              <Typography>
+                {convertToLongDateFromShortDate(deleteEntryDate)}
+              </Typography>
+              <ModalButtonsContainer>
+                <Button variant="contained" onClick={() => handleModalClose()}>
+                  No, take me back
+                </Button>
+                <Button
+                  color="error"
+                  onClick={() => handleDeleteEntry(deleteEntryDate)}
+                >
+                  Yes, Delete
+                </Button>
+              </ModalButtonsContainer>
+            </Box>
+          </Modal>
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={6000}
+            onClose={handleSnackbarClose}
+          >
+            <Alert onClose={handleSnackbarClose} severity="success">
+              {snackbarMessage}
+            </Alert>
+          </Snackbar>
+          {isLoading && <LoadingComponent />}
+        </EntriesContainer>
+      </ContentContainer>
     </PageContentContainer>
   )
 }
